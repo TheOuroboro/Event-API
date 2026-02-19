cf50d7fd9c7c0d64afc1af8ed9a894533c30fb97
Abdulfatai Adam <nbq314@gmail.com>
2026-02-19 14:49:37 +0100

    soleved render issues, ready for deployment now

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 const { prisma } = require('../config/database');
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const {} = require ("../utils/password")
 const { generateToken } = require('../utils/jwt');
