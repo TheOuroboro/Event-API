0751b7b0bbf14be72f02fd083692eca557fc92d2
Abdulfatai Adam <nbq314@gmail.com>
2026-02-20 16:25:15 +0100

    forgot the joi validatkion for name, now removed

--- src/validators/authValidator.js
+++ src/validators/authValidator.js
@@ -2,7 +2,6 @@ const Joi = require('joi');
 
 const registerSchema = (data) => {
   const schema = Joi.object({
-    name: Joi.string().min(3).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
     role: Joi.string().valid('ORGANIZER', 'ATTENDEE').default('ATTENDEE')
